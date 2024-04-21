8af89cdd486256f8a220cd3ab2dfae3a724d7d7f
thiagovieiras <thiago.santos@sptech.school>
2024-04-21 19:52:59 -0300

    arrumei o tamanho das div e o estilo do scroll

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const check1 = document.getElementById('check_1');
+const check2 = document.getElementById('check_2');
+const check3 = document.getElementById('check_3');
+
+const container = document.getElementById('container');
+
+// if (check1.checked) {
+//     check2 = //função para deixar 45%-60% transparente, e bloquear o acesso do usuario; 
+//     check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
+
+// } else if (check2.checked) {
+//     check2 = //função mudar a cor do check e permite voltar para o check1
+//     check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
+
+// } else {
+//     check3 = //função mudar a cor do check e permite voltar para o check1 e check2
+
+// };
+
