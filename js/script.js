671c302efebc8b250bf8126eb7fec7917a02a482
thiagovieiras <thiago.santos@sptech.school>
2024-04-21 20:01:25 -0300

    fiz o layout na função

--- js/script.js
+++ js/script.js
@@ -4,16 +4,47 @@ const check3 = document.getElementById('check_3');
 
 const container = document.getElementById('container');
 
-// if (check1.checked) {
-//     check2 = //função para deixar 45%-60% transparente, e bloquear o acesso do usuario; 
-//     check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
+if (check1.checked) {
 
-// } else if (check2.checked) {
-//     check2 = //função mudar a cor do check e permite voltar para o check1
-//     check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
+    layoutInserir();
+    // check2 = //função para deixar 45%-60% transparente, e bloquear o acesso do usuario; 
+    // check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
 
-// } else {
-//     check3 = //função mudar a cor do check e permite voltar para o check1 e check2
+} else if (check2.checked) {
+    // check2 = //função mudar a cor do check e permite voltar para o check1
+    // check3 = //função para deixar 60%-80% transparente, e bloquear o acesso do usuario;
 
-// };
+} else {
+    // check3 = //função mudar a cor do check e permite voltar para o check1 e check2
 
+};
+
+
+function layoutInserir() {
+    container.innerHTML = `
+    <div class="rotulo">
+        <p>Escreva os itens</p>
+        <span>*Separados por linha</span>
+    </div>
+    <textarea id="input" wrap="soft"></textarea>
+    <div class="rotulo">
+        <p>Upload do arquivo</p>
+        <span>*.CSV</span>
+    </div>
+    <div class="arquivo">
+        <label for="iptArquivo">
+            Escolher Arquivo
+            <input type="file" id="iptArquivo">
+        </label>
+    </div>
+    <button>Continuar</button>
+    `
+}
+
+function layoutQuantidade() {
+    container = `
+    AI VOCE DESENROLA DESSE EM DIANTE
+
+    USA O LAYOUT DA FUNÇÂO ANTERIOR
+    `
+}
